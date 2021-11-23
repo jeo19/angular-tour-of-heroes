7e62fb464bf2809f93ab02cd167807ace2724810
jeo19 <terrakkk@outlook.com>
2021-11-23 11:44:32 +0800

    feat:Add the heroes service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero' 
-import { HEROES } from '../mock-heroes';
+import {Hero} from '../hero'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,15 +12,19 @@ export class HeroesComponent implements OnInit {
     id:1,
     name:'windStorm'
   }
-  heroes=HEROES;
+  heroes:Hero[]=[];
   selectedHero?:Hero;
   onSelect(hero:Hero):void{
     this.selectedHero=hero;
   }
 
-  constructor() { }
+  constructor(private heroService:HeroService) { }
+  getHeroes():void{
+    this.heroes=this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
