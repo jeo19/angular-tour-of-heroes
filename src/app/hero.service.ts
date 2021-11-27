6b9dffedd6e1335f573dc531a148856dd8c8fe75
jeo19 <terrakkk@outlook.com>
2021-11-28 02:02:03 +0800

    feat:Refactory the heroes-detail component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,8 +12,11 @@ export class HeroService {
   constructor(private messageService:MessageService) { }
   getHeroes():Observable< Hero[]>{
     const heroes=of(HEROES);
-    this.messageService.add('HeroService:fetched heroes');
     return heroes;
-
+  }
+  getHero(id:Number):Observable<Hero>{
+    const hero=HEROES.find(h=>h.id===id)!;
+    this.messageService.add(`HeroService:fetched heroid=${id}`)
+    return of(hero);
   }
 }
