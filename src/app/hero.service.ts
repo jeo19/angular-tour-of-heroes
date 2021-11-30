fc498d482f0ed360d9813d60597fb97be1ff4b3b
jeo19 <terrakkk@outlook.com>
2021-11-30 09:16:27 +0800

    feat:Add the error handler

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,20 +3,49 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable,  of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService:MessageService) { }
-  getHeroes():Observable< Hero[]>{
-    const heroes=of(HEROES);
-    return heroes;
+  private log(message:string){
+    this.messageService.add(`HeroService:${message}`);
   }
-  getHero(id:Number):Observable<Hero>{
-    const hero=HEROES.find(h=>h.id===id)!;
-    this.messageService.add(`HeroService:fetched heroid=${id}`)
-    return of(hero);
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+    getHeroes(): Observable<Hero[]> {
+      return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+    }
+    getHero(id: number): Observable<Hero> {
+      const url = `${this.heroesUrl}/${id}`;
+      return this.http.get<Hero>(url).pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
   }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO:Sending error message to the remote server
+      console.error(error); 
+  
+      // TODO: Convert to type that user can understand
+      this.log(`${operation} failed: ${error.message}`);
+  
+      //The basic vaule of error
+      return of(result as T);
+    };
+  }
+
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,10 +3,10 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-  createDb(){
+  createDb() {
     const heroes = [
       { id: 11, name: 'Dr Nice' },
       { id: 12, name: 'Narco' },
@@ -19,10 +19,10 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return {heroes}
+    return {heroes};
   }
- genId(heroes:Hero[]):number{
-   return heroes.length > 0 ? Math.max(...heroes.map(hero=>hero.id))+1 :11;
- }
-  constructor() { }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
 }
