8289a5346a0083f8c2671cfc38cbb3da677b2c48
jeo19 <terrakkk@outlook.com>
2021-12-02 08:33:09 +0800

    feat:Implement the feature that to add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -43,6 +43,12 @@ export class HeroService {
         catchError(this.handleError<any>('update?Hero'))
       )
     }
+    addHero(hero:Hero):Observable<Hero>{
+      return this.http.post<Hero>(this.heroesUrl,hero,this.httpOptions).pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      )
+    }
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
   
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,5 +20,10 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
+  add(name:String):void{
+    name=name.trim();
+    if(!name) { return ;}
+    this.heroService.addHero({name} as Hero).subscribe(hero=>{ this.heroes.push(hero);});
+  }
 
 }
