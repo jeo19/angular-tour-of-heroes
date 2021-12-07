09e8d011fdaaab0ae49f9d3857104310ffa2e5e9
jeo19 <terrakkk@outlook.com>
2021-12-07 10:17:32 +0800

    fear:Implement the hero-search component

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import {Hero} from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$!:Observable<Hero[]>;
+  private searchTerms=new Subject<string>();
 
-  constructor() { }
+
+  constructor(private heroService:HeroService) { }
+  search(term:string):void {
+    this.searchTerms.next(term)
+
+
+  }
 
   ngOnInit(): void {
+    this.heroes$=this.searchTerms.pipe(
+      //wait for 30s in order to proccessed sequenced a data
+      debounceTime(300),
+      //ignore if data equal previously endtered search term
+      distinctUntilChanged(),
+      //if the search term changed,it will create new observale
+      switchMap((term:string)=>this.heroService.searchHeroes(term)),
+    );
   }
 
 }
